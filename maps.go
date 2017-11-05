262b25eea556646a3eb7bc095294ad3971118b87
Srikanth Lankapalli <machine.progsri@gmail.com>
2017-11-04 23:07:25 -0500

    delete a key

--- maps.go
+++ maps.go
@@ -4,10 +4,20 @@ import "fmt"
 
 func main() {
 
+	fmt.Println("-------- create a map -------------")
 	map_1 := make(map[string]int)
 
 	map_1["con"] = 1
 
 	fmt.Println("map ", map_1)
+
+	map_1["key2"] = 2
+
+	fmt.Println("map ", map_1)
+
+	delete(map_1, "key2")
+
+	fmt.Println("map ", map_1)
+
 
 }
