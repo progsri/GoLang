949ad74b379bb0e04d782ea8d7c384c5531c86e3
progsri <machine.progsri@gmail.com>
2017-11-05 17:37:47 -0600

    formatting with logs

--- maps.go
+++ maps.go
@@ -4,20 +4,21 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("-------- create a map -------------")
+	fmt.Println("--------------------")
 	map_1 := make(map[string]int)
 
-	map_1["con"] = 1
+	map_1["key1"] = 1
 
-	fmt.Println("map ", map_1)
+	fmt.Println("adding first key to map >> ", map_1)
 
 	map_1["key2"] = 2
 
-	fmt.Println("map ", map_1)
+	fmt.Println("adding a second key to map >> ", map_1)
 
 	delete(map_1, "key2")
 
-	fmt.Println("map ", map_1)
+	fmt.Println("deleting key from a map >> ", map_1)
 
+	
 
 }
