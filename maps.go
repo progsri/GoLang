7d32ebc2a3fc3bed417bcc92a308208420e046c6
progsri <machine.progsri@gmail.com>
2017-11-06 17:07:25 -0600

    2 values for a map key

--- maps.go
+++ maps.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("--------------------")
+	fmt.Println("-------creating map-------------")
 	map_1 := make(map[string]int)
 
 	map_1["key1"] = 1
@@ -19,6 +19,11 @@ func main() {
 
 	fmt.Println("deleting key from a map >> ", map_1)
 
-	
+        fmt.Println("------- map's key returns multi values ---------------")
 
+	map_1["key3"] = 0
+	v1, v2 := map_1["key4"]
+
+	fmt.Println("first is the value of key >> ", v1)
+	fmt.Println("second is the existence of the key >> ", v2)
 }
