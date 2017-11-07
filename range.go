bdb2ac9e69b0f87e140d49938cddb259f5e77523
Srikanth Lankapalli <machine.progsri@gmail.com>
2017-11-07 14:28:02 -0600

    range for slice and string

---
+++ range.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main(){
+	
+	fmt.Println("-------------- slices -----------------")
+	slice := make([]string, 6)
+	slice[0] = "a"
+	slice[2] = "b"
+	slice[5] = "c"
+
+	fmt.Println(" slice " , slice)
+	
+	for v1, v2 := range slice {
+		fmt.Println("First Value [index] >> ", v1)
+		fmt.Println("Second Value [value] >> ", v2)
+	}
+
+	fmt.Println("------------------string -----------------")
+
+	var word string = "this is a sentence"
+	for v3, v4 := range word{
+		fmt.Println("First Value [index] >> ", v3)
+		fmt.Println("Second Value [value] >> ", v4)
+	}
+	
+}
