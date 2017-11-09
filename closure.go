bd1259b77e45a48ffcdde4b529e5be4540899b1f
Srikanth Lankapalli <machine.progsri@gmail.com>
2017-11-09 11:29:18 -0600

    closure variable scope

--- closure.go
+++ closure.go
@@ -16,12 +16,17 @@ func main() {
 
 }
 
+//Regular method ... call starts and ends sequentially...once ended
+// the variables created locally are out of scope.
+// Closure ... since the reference to it always exists the current value of the 
+//variable will continue to exists ( in the scope ) even after the closure method is done executing.
+
 func func_returns_func() func() int{
 	
 	value := 1
 	fmt.Println("outer func value ", value)
 	return  func() int {
-		value = value + 1
+		value = value + 1     // this value gets updated..will not be the save always
 		fmt.Println("inner func value ", value)
 		return value
 	}
