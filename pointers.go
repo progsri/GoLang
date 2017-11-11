1e566bdaedb14e5002824e36441d3877749e9f67
Srikanth Lankapalli <machine.progsri@gmail.com>
2017-11-11 15:49:06 -0600

    getting the address

--- pointers.go
+++ pointers.go
@@ -6,15 +6,21 @@ func main() {
 
 	//  variable of type int holds int value
 	i := 3
+
 	fmt.Println("value of i >> ", i )
-
-	fmt.Println("address of i >> ", &i)
-
-	// variable of type *int holds address of the variable
+    fmt.Println("address of i >> ", &i)
 	i_ptr  := &i
-
 	fmt.Println("pointer(address) of i >> ", i_ptr)
+	fmt.Println("value of pointer >> ", *i_ptr)
+	
+	*i_ptr = 4
+	fmt.Println("-----------------------------------")
+	fmt.Println("changing the value of the address >> ", i)
 
-        fmt.Println("value of pointer >> ", *i_ptr)
+	fmt.Println("value of i >> ", i )
+    fmt.Println("address of i >> ", &i)
+    fmt.Println("pointer(address) of i >> ", i_ptr)
+	fmt.Println("value of pointer >> ", *i_ptr)
+
 
 }
