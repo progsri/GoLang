5fbc6c22d42d0f7d310e38dd65832e4d376247d5
Srikanth Lankapalli <machine.progsri@gmail.com>
2017-11-11 20:05:47 -0600

    access and update

--- structs.go
+++ structs.go
@@ -5,7 +5,7 @@ import "fmt"
 type group struct {
 	name string
 	count int
-	subgroup subgroup
+	sub subgroup
 }
 
 type subgroup struct {
@@ -14,11 +14,20 @@ type subgroup struct {
 }
 
 func main() {
-	
+
+	fmt.Println("-------------creating a struct ---------")
 	subg1 := subgroup{"s1","sbg1"}
 	fmt.Println(subg1)
 	g1 := group{"g1", 1, subg1}
 	fmt.Println(g1)
-	
+
+	fmt.Println("---------access struct ----------------")
+	fmt.Println("directly >>> ", g1.name)
+
+	g1_ptr := &g1
+	fmt.Println("pointer >> ", g1_ptr.name)
+
+	g1_ptr.sub.id = "test"
+	fmt.Println("update >>", g1)
 }
 
