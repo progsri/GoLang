8cd8a5bb0fb1c67bc0545dbeef9cfbfece16296a
Srikanth Lankapalli <machine.progsri@gmail.com>
2017-11-15 13:12:43 -0600

    struct method 2

--- struct_method.go
+++ struct_method.go
@@ -11,6 +11,14 @@ func main(){
 	m_struct_ptr := &math{100, 200}
 	fmt.Println("struct w ptr >> ", *m_struct_ptr)
 	fmt.Println("struct w ptr add >> ", m_struct_ptr.add())  // Go automatically converts the pointer to a value(struct)
+
+	m_struct_2 := math{3,4}
+	fmt.Println("struct w/o ptr >> ", m_struct_2)
+	fmt.Println("struct w/o ptr multiply >> ", m_struct_2.multiply())
+
+	m_struct_2_ptr := &math{23,3}
+	fmt.Println("struct w ptr >> ", *m_struct_2_ptr)
+	fmt.Println("struct w ptr multiply >> ", m_struct_2_ptr.multiply())
 }
 
 type math struct{
@@ -23,6 +31,6 @@ func (m math) add() int {
 	return m.first_value + m.second_value
 }
 
-func (m *math) multiply int {
-	return  *m.first_value * *m.second_value
+func (m *math) multiply() int {
+	return  m.first_value * m.second_value
 }
