3f123bf0d1aae36255b04c91cae6e397122a1c66
Srikanth Lankapalli <machine.progsri@gmail.com>
2017-11-20 10:12:16 -0600

    go routine ...works now

--- goroutine.go
+++ goroutine.go
@@ -1,22 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 
-	fmt.Println("------------ reg--------------")
-	//regularMethod("regular")
+	fmt.Println("------------ reg --------------")
 
-	go regularMethod("go routine 1")
+	regStart := time.Now().Nanosecond()
+	regularMethod("regular") // Synchronous
+	regStop := time.Now().Nanosecond()
+	fmt.Println(regStop - regStart)
+
+	fmt.Println("------------ goroutine --------------")
+
+	goStart := time.Now().Nanosecond()
+	go regularMethod("go routine 1")  // Asynchronous
+	go regularMethod("go routine 2")  // Asynchronous
+	go regularMethod("go routine 3")  // Asynchronous
+	go regularMethod("go routine 4")  // Asynchronous
+	go regularMethod("go routine 5")  // Asynchronous
+	go regularMethod("go routine 6")  // Asynchronous
+	go regularMethod("go routine 7")  // Asynchronous
+	go regularMethod("go routine 8")  // Asynchronous
+	go regularMethod("go routine 9")  // Asynchronous
+	go regularMethod("go routine 10") // Asynchronous
+	goStop := time.Now().Nanosecond()
+	fmt.Println(goStop - goStart)
 
 	// to wait for the go rountine to finish execution
-	fmt.Scanln("waiting for go routine to finish execution .... this is a hack.")
+	var value string
+	fmt.Scanln(&value)
 
 	fmt.Println("------------ end --------------")
 }
 
 func regularMethod(caller string) {
-	for i := 0; i < 10000; i++ {
+	for i := 0; i < 10; i++ {
 		fmt.Printf(" %s - %d \n ", caller, i)
 	}
 }
