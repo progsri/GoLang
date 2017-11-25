0bf21786e1e7b73a3f7a7c42970273afe72aee4b
Srikanth Lankapalli <machine.progsri@gmail.com>
2017-11-25 16:39:47 -0600

    very basic channel

--- channel.go
+++ channel.go
@@ -8,13 +8,13 @@ func main() {
 
 	channel1 := make(chan string)
 
-	// channel1 <- "main before method 1" // will throw error : all go routines are asleep ... since this is blocking call and waits to recive data with in a
+	// channel1 <- "main before method 1" // will throw error : all go routines are asleep ... since this is blocking call and waits to assign data with in a
 	// goroutine as channels are only used via goroutines.
 	go method1(channel1)
 
-	fmt.Println(<-channel1)
+	fmt.Println(<-channel1) // we can always access the data from a channel.
 }
 
 func method1(channel1 chan string) {
-	channel1 <- "method1"
+	//channel1 <- "method1"
 }
