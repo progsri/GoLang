b7c3dd1aec96c5d8cdb444aadc340faf04480b98
Srikanth Lankapalli <machine.progsri@gmail.com>
2017-11-29 12:08:14 -0600

    Non blocking channels

---
+++ channelNonBlocking.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	channel1 := make(chan string)
+	go sendTo(channel1)
+	fmt.Println("blocking received >>", <-channel1) //blocking mode
+
+	//non blocking mode  receiving
+	go sendTo(channel1)
+	select {
+	case <-channel1:
+		fmt.Println("non blocking >>  received")
+	default:
+		fmt.Println("non blocking >> did not receive")
+	}
+
+	//non blocking receive
+	go sendToNonBlocking(channel1)
+
+	fmt.Println("Wait for user Input")
+	var input string
+	fmt.Scanln(&input)
+}
+
+func sendTo(channel1 chan string) {
+	time.Sleep(time.Second * 3)
+	channel1 <- "sending to channel"
+	fmt.Println("blocking >> sent to channel")
+}
+
+func sendToNonBlocking(channel1 chan string) {
+	//There is ono receiver
+	//select {
+	channel1 <- "sending to non blocking channel"
+	channel1 <- "sending to non blocking channel"
+	//}
+	fmt.Println("non blocking >> sent to channel")
+}
