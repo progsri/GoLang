e07b8b9b54aef2a1c894f05d815c490d9d2ce1ea
Srikanth Lankapalli <machine.progsri@gmail.com>
2017-12-02 19:03:07 -0600

    Understanding timer channel

--- timer.go
+++ timer.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
+
+	timer1 := time.NewTimer(time.Second * 2)
+	<-timer1.C
+	fmt.Println("Timer 1 has completed")
+
+	timer2 := time.NewTimer(time.Second)
+	go func() {
+		<-timer2.C
+		fmt.Println("Timer 2 has completed")
+	}()
+
+	fmt.Println("Waiting for the input")
+	var input string
+	fmt.Scanln(&input)
 
 }
