a949d49bccde51583b5ea106830871cc3f7e4a95
Srikanth Lankapalli <machine.progsri@gmail.com>
2017-12-08 22:46:25 -0600

    Non Working code ... will revisit later.

--- statefulGoroutines_notWorking.go
+++ statefulGoroutines_notWorking.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"time"
+	"strconv"
 )
 
 func main() {
@@ -17,16 +18,17 @@ func main() {
 	fmt.Println("Without locks ", count)  // this count will alwaysbe less 100 due to race conditions.
 
 	channelRequests := make(chan int, 100)
-	channelResponse := make(chan int)
+	channelResponse := make(chan int, 100)
 
 	go manageLock(channelRequests, channelResponse)
 	
-	countWithLock := 0
-	for i := 0; i < 100; i++ {
+
+	for i := 0; i < 5; i++ {
 		go increment2(channelRequests)
 	}
 
-	time.Sleep(time.Second * 5)  // wait for the above goroutines to complete
+	time.Sleep(time.Second * 50)  // wait for the above goroutines to complete
+	countWithLock := <- channelResponse
 	fmt.Println("With locks ", countWithLock)
 
 	fmt.Println("waiting for input") 
@@ -41,11 +43,22 @@ func increment1(count *int) {
 
 func increment2(channel1 chan int) {
 	channel1 <- 1
-}
+	fmt.Println("Sending 1 to channel")
+	}
 
 func manageLock(channel1 chan int, channel2 chan int){
 
-	for i:= range channel2{
+	count := 0
 
+	for {
+		fmt.Println(" Blocking >> ")
+		i := <- channel1
+		fmt.Println("Recevied >> " + strconv.Itoa(i))
+		if i > 0 {
+			count = count + i
+		}
+
+		fmt.Println("sending to response channel count >> " + strconv.Itoa(count))
+		channel2 <- count
 	}
 }
