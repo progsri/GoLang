d2e5d62e11a8c668c795bc3d28f40b699484de82
srikanth <machine.progsri@gmail.com>
2018-12-28 06:40:49 -0600

    value based

--- array.go
+++ array.go
@@ -2,17 +2,26 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 
 	var arr [10]string
 	arr[0] = "a"
 	arr[1] = "b"
 
-	for i, err := range arr{
+	for i, err := range arr {
 		if err != "" {
 			fmt.Println("error occurred")
 		}
-		fmt.Println(arr[i] )
+		fmt.Println(arr[i])
 	}
 
-	}
+	 arr1 := [...]string{"a","b","c"}
+	 fmt.Println("before sending arr1 values ", arr1)
+	 m1(arr1)
+	 fmt.Println("after sending arr1 values ", arr1) // So Passing arraying is value based not reference based
+}
+
+func m1(arr [3]string) {
+	arr[0] = "modfiying"
+	fmt.Println(" arr is func ", arr)
+}
