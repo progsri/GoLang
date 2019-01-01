a531f7b6a56a52f95df2e37f735dd4246eca7bc2
srikanth <machine.progsri@gmail.com>
2019-01-01 09:49:41 -0600

    Comparing 2 different types

--- struct_equality.go
+++ struct_equality.go
@@ -7,6 +7,11 @@ type Person struct {
 	zip  int
 }
 
+type PersonAnother struct {
+	name string
+	zip  int
+}
+
 func main() {
 	p1 := Person{
 		"first", 111,
@@ -32,5 +37,14 @@ func main() {
 		fmt.Println("Not Equal")
 	}
 
-	
+	// p4 := PersonAnother{
+	// 	"first", 111,
+	// }
+
+	//if p1 == p4 {   //wow :: Go does not even let you to compile
+	//	fmt.Println("EQUAL")
+	//} else {
+	//	fmt.Println("Not Equal")
+	//}
+
 }
