3f89dcc938543a677b552f33212ab6cdcfa2667e
srikanth <machine.progsri@gmail.com>
2019-01-07 07:32:57 -0600

    Adding new methods to a existing struct from a different package - composition

--- p1/entry.go
+++ p1/entry.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"GoLang/p1/p2"
 	"GoLang/p1/p2/p3"
 	"fmt"
 )
@@ -10,5 +11,10 @@ func main() {
 	e1 := p3.Employee{Id: "e1", Email: "e1@gmail.com"}
 	fmt.Println(e1)
 	e1.Skills()
-//	fmt.Println(e1.Apply()) // This would not work as the Apply method is defined in a different package.
+	//	fmt.Println(e1.Apply()) // This would not work as the Apply method is defined in a different package.
+
+	s1 := p2.SubEmployee{e1}
+
+	fmt.Println("Sub Employee ", s1.Apply())
+
 }
--- p1/p2/eligible.go
+++ p1/p2/eligible.go
@@ -12,7 +12,21 @@ func (e emp) apply() int { // This would not work as the  method is defined in a
 	return 20
 }
 
-func (e emp) Apply() int {// This would not work as the Apply method is defined in a different package rather than on Employee package.
+func (e *emp) Apply() int { // This would not work as the Apply method is defined in a different package rather than on Employee package.
 	fmt.Println(e.Email)
+	return 20
+}
+
+type SubEmployee struct {
+	p3.Employee
+}
+
+func (s *SubEmployee) apply() int { // This would not work as the  method is defined in a different package rather than on Employee package.
+	fmt.Println(s.Email)
+	return 20
+}
+
+func (s *SubEmployee) Apply() int { // This would not work as the Apply method is defined in a different package rather than on Employee package.
+	fmt.Println("Sub Employee Email ", s.Email)
 	return 20
 }
