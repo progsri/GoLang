a054e87aa4e69b41a45f9f9e0b542067f493d4c6
srikanth <machine.progsri@gmail.com>
2019-01-15 06:19:24 -0600

    adding pointer declaration

--- pointers.go
+++ pointers.go
@@ -4,13 +4,13 @@ import "fmt"
 import "reflect"
 
 type group1 struct {
-	name string
+	name  string
 	count int
-	sub subgroup1
+	sub   subgroup1
 }
 
 type subgroup1 struct {
-	id string
+	id   string
 	desc string
 }
 
@@ -19,23 +19,23 @@ func main() {
 	//  variable of type int holds int value
 	i := 3
 	fmt.Println("type of i ", reflect.ValueOf(i).Kind())
-	fmt.Println("value of i >> ", i )
-    fmt.Println("address of i >> ", &i)
+	fmt.Println("value of i >> ", i)
+	fmt.Println("address of i >> ", &i)
 
-	i_ptr  := &i
+	var i_ptr *int = &i //( indicates pointer of type int )
 	fmt.Println("type of i_ptr ", reflect.ValueOf(i_ptr).Kind())
 	fmt.Println("pointer(address) of i >> ", i_ptr)
-	fmt.Println("pointer(address) of i >> ", &i_ptr) //?? What is this actually giving
+	fmt.Println("pointer(address) of i >> ", &i_ptr) //gives the address of the pointer
 	fmt.Println("value of pointer >> ", *i_ptr)
-	
+
 	*i_ptr = 4
 	fmt.Println("-----------------------------------")
 	fmt.Println("type of i_ptr ", reflect.ValueOf(i_ptr).Kind())
 	fmt.Println("changing the value of the address >> ", i)
 
-	fmt.Println("value of i >> ", i )
-    fmt.Println("address of i >> ", &i)
-    fmt.Println("pointer(address) of i >> ", i_ptr)
+	fmt.Println("value of i >> ", i)
+	fmt.Println("address of i >> ", &i)
+	fmt.Println("pointer(address) of i >> ", i_ptr)
 	fmt.Println("value of pointer >> ", *i_ptr)
 
 	fmt.Println("--------------------")
@@ -48,23 +48,23 @@ func main() {
 
 	fmt.Println("--------------------")
 
-	subg1 := subgroup1{"s1","sbg1"}
+	subg1 := subgroup1{"s1", "sbg1"}
 	g1 := group1{
-		name : "g1",
-		count : 1,
-		sub : subg1}
-	fmt.Println("g1 " , g1)
+		name:  "g1",
+		count: 1,
+		sub:   subg1}
+	fmt.Println("g1 ", g1)
 	fmt.Println("type of g1 ", reflect.ValueOf(g1).Kind())
 	fmt.Println("type of &g1  ", &g1)
 
 	fmt.Println("--------------------")
 
-	subg2 := subgroup1{"s1","sbg1"}
+	subg2 := subgroup1{"s1", "sbg1"}
 	g2 := &group1{
-		name : "g1",
-		count : 1,
-		sub : subg2}
-	fmt.Println("g2 " , g2)
+		name:  "g1",
+		count: 1,
+		sub:   subg2}
+	fmt.Println("g2 ", g2)
 	fmt.Println("type of g2 ", reflect.ValueOf(g2).Kind())
 	fmt.Println("type of &g2 ", &g2)
 
