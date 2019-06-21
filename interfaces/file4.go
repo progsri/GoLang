a16ec086b6bf92ed39e8242f6b0c7f25b9506d65
Srikanth Lankapalli <machine.progsri@gmail.com>
2019-06-21 09:10:08 -0500

    Update file4.go

--- interfaces/file4.go
+++ interfaces/file4.go
@@ -3,29 +3,59 @@ package main
 import "fmt"
 
 type config struct {
-	addressNop i1
+	addressNop i1        //This can take a pointer struct or a struct
 	addressP   i1
 	network    string
+	raw1 	rawConfig   //This can take only a struct
+	raw2	*rawConfig   //This can take only a pointer struct
+}
+
+type rawConfig struct{
+	key1 string
 }
 
 func main() {
 
-	structNop := S1{
+	structNop1 := S1{
 		"s1-nop",
 	}
-	fmt.Println(structNop)
+	fmt.Println(structNop1)
 
-	structP := S1{
+	structP1 := S1{
 		"s1-p",
 	}
-	fmt.Println(structP)
+	fmt.Println(structP1)
 
-	structConfig := config{
-		addressNop: structNop,
-		addressP:   structP,
-		network: "tcp",
+	structNop2 := &S1{
+		"s1-nop",
+	}
+	fmt.Println(structNop2)
+
+	structP2 := &S1{
+		"s1-p",
+	}
+	fmt.Println(structP2)
+
+	raw1 := rawConfig{
+		key1: "key1",
 	}
 
-	fmt.Println(structConfig)
+	raw2 := &rawConfig{
+		key1: "key2",
+	}
+
+	structConfig1 := config{
+		addressNop: structNop1,
+		addressP:   structP1,
+		network: "tcp",
+		raw1: raw1,  
+		raw2: raw2,
+	}
+
+	structConfig2 := config{}  //This pointers default to nil and others to zero values
+
+	fmt.Println(structConfig1)
+	fmt.Println(structConfig2)
+
 
 }
