61904f766820bb0c4e6caf968e70d1695b291b33
Srikanth Lankapalli <machine.progsri@gmail.com>
2019-06-27 11:40:32 -0500

    Update array.go

--- array.go
+++ array.go
@@ -20,6 +20,7 @@ func main() {
 	 m1(arr1)
 	 fmt.Println("after sending arr1 values ", arr1) 
 	 // So Passing arraying is value based not reference based..unlike maps which is referenced based
+	
 }
 
 func m1(arr [3]string) {
