5f9916d77d8daf294288d4f7e70e93266768e805
Srikanth Lankapalli <machine.progsri@gmail.com>
2019-06-27 11:46:27 -0500

    Update Index.go

--- GoBuiltInPackages/ErrorsPackages/Index.go
+++ GoBuiltInPackages/ErrorsPackages/Index.go
@@ -1,1 +1,15 @@
-A
+package main
+
+import "errors"
+import "fmt"
+
+func main() {
+
+	// Lets you create a errorString struct
+	s1 := errors.New("error 1")
+	fmt.Println(s1)
+
+	// Gives you access to the struct method Error()
+	fmt.Println(s1.Error())
+}
+
